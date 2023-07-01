c6340d41e2623246a339c450337c571f840193aa
Kkr736549 <137636896+Kkr736549@users.noreply.github.com>
2023-07-01 14:59:52 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(my_fruit_list)
                       
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
