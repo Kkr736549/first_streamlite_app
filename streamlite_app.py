54ef1f43cf3763fb6d508c1a23027a1be7e1fe85
Kkr736549 <137636896+Kkr736549@users.noreply.github.com>
2023-07-06 20:21:37 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector 
-from urllib.error import URLError
+#import pandas
+#import requests
+#import snowflake.connector 
+#from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -13,7 +13,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# import pandas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -29,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered ', fruit_choice)
 
-# import requests
+ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version respons and normaliz it
@@ -54,4 +54,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfrui
 streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
                      
 # this will not work but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit') ") 
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")  
