4165fea8a755d78a74dc78c55e7e169d35cc1105
Kkr736549 <137636896+Kkr736549@users.noreply.github.com>
2023-07-06 09:22:56 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -20,26 +20,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)),['Avocado'
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list) 
                       
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
