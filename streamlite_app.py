f9aa2ed0a37189e7f94c5ff693aba76ea25e2c2e
Kkr736549 <137636896+Kkr736549@users.noreply.github.com>
2023-07-07 10:17:55 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -8,7 +8,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
-streamlit.text('🥗Kale,Spanich & rocket Smoothie')
+streamlit.text('🥗Kale,Spanich & rocket Smoothie') 
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
