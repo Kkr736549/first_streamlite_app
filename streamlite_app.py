fbd706d7eb17e915325b2aa62cea9970116be665
Kkr736549 <137636896+Kkr736549@users.noreply.github.com>
2023-07-07 10:17:15 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -49,7 +49,7 @@ streamlit.stop()
 
 # snowflake related function
 def get_fruit_load_list():
-     with  my_cnx.cursor()asmy_cur:
+     with  my_cnx.cursor() as my_cur:
            my_cur.execute("SELECT * from fruit_load_list")
            return my_cur.fetchall()
 # add a button to a load fruit
